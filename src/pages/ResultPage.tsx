75f3f2fa33fe2cc0fd7e861a0b6a1adb3b4e75d6
1z3r091 <1z3r091@gmail.com>
2023-03-06 12:22:26 +0900

    fix/ 사용안하는 컴포넌트 import 제거

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button, Image } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import { useSearchParams } from 'react-router-dom';
 
 import { resultData } from '../stores/Result/resultData';
